ef6f674fb5cea1e8374cf816dc77f1ac10999eec
Aishwarya Iyer <37527519+ashviyer@users.noreply.github.com>
2026-03-06 16:03:27 +0100

    Refactor run_stability.py for dynamic file paths
    Updated file paths and added functionality to save stability plots.

--- scripts/pyNBS/run_stability.py
+++ scripts/pyNBS/run_stability.py
@@ -2,27 +2,33 @@
 
 import pandas as pd
 from pyNBS.pyNBS_stability import full_stability_report
+from pyNBS.pyNBS_stability import save_all_stability_plots
 
-cc_table = pd.read_csv("pynbs_results/myjob_cc_matrix.csv", index_col=0)
+cc_file = "pynbs_results/YOUR_CC_MATRIX.csv"
+cluster_file = "pynbs_results/YOUR_CLUSTER_ASSIGNMENTS.csv"
+prefix = "YOUR_PREFIX"
+
+cc_table = pd.read_csv(cc_file, index_col=0)
 
 cluster_assign = pd.read_csv(
-    "pynbs_results/myjob_cluster_assignments.csv",
+    cluster_file,
     index_col=0,
     squeeze=True
 )
-
-# If squeeze=True does not behave as expected in your pandas version,
-# use this instead:
-# cluster_assign = pd.read_csv("pynbs_results/myjob_cluster_assignments.csv", index_col=0).iloc[:, 0]
-
 cluster_assign.name = "cluster"
 
 report = full_stability_report(
     cc_table=cc_table,
     cluster_assign=cluster_assign,
     outdir="pynbs_results/stability",
-    prefix="myjob",
+    prefix=prefix,
     do_silhouette=True
+)
+
+save_all_stability_plots(
+    report=report,
+    outdir="pynbs_results/stability",
+    prefix=prefix
 )
 
 print report["global_consensus"]
